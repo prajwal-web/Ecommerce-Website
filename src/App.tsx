c8e11f99b7952e3754b5b9a8b4d6f4fe72cb750b
Prajwal V <prajuu2000@gmail.com>
2025-02-18 12:07:14 +0530

    feat: saving changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
       <h1>hello there</h1>
       <Typography variant="h1">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, blanditiis ad! Velit, corporis eveniet explicabo
-        minus eum neque rem.
       </Typography>
       <Button variant="contained">click</Button>
       <Typography variant="body1">Whereas recognition of the inherent dignity</Typography>
