e9c1eabdc55dbdd13288e61743ec1480b419bc71
Prajwal V <prajuu2000@gmail.com>
2025-02-18 17:30:16 +0530

    feat: saving changes

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,7 @@
-export default {
+module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // TODO Add Scope Enum Here
+    //   TODO Add Scope Enum Here
     // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
     'type-enum': [
       2,
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { Button, ThemeProvider, Typography } from '@mui/material';
+import { ThemeProvider, Button } from '@mui/material';
 import theme from './theme/theme';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <h1>hello there</h1>
-      <Typography variant="h1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, blanditiis ad! Velit, corporis eveniet explicabo
-      </Typography>
-      <Button variant="contained">click</Button>
-      <Typography variant="body1">Whereas recognition of the inherent dignity</Typography>
+      <Button variant="contained"> click </Button>
     </ThemeProvider>
   );
 }
