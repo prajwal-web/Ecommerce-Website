2e91679acccbd012cc7d601fd4deefe23d66ab0a
Prajwal V <prajuu2000@gmail.com>
2025-02-18 10:25:53 +0530

    theme setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Button, ThemeProvider, Typography } from '@mui/material';
+import theme from './theme/theme';
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <h1>hello there</h1>
-    </>
+      <Typography variant="h1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, blanditiis ad! Velit, corporis eveniet explicabo
+        minus eum neque rem.
+      </Typography>
+      <Button variant="contained">click</Button>
+      <Typography variant="body1">Whereas recognition of the inherent dignity</Typography>
+    </ThemeProvider>
   );
 }
 
