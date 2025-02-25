86bb6ab913a60a0a97ba8e877eba573959d4bfaa
Prajwal V <prajuu2000@gmail.com>
2025-02-25 17:47:52 +0530

    feat: setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Button, ThemeProvider, Typography } from '@mui/material';
 import theme from './theme';
+import { Camera } from 'lucide-react';
+import { Icon } from 'lucide-react';
+import { burger } from '@lucide/lab';
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Typography variant="h1">hello world</Typography>
       <Button variant="contained">click here</Button>
+      <Camera color="red" size={48} />
+      <Icon iconNode={burger} />
     </ThemeProvider>
   );
 }
